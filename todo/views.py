ce2d23525e1bbdf6c70d8a83ed48987f39c647e1
Fernan043 <feartag2710@gmail.com>
2025-03-27 21:55:14 -0600

    Login funcional

--- todo/views.py
+++ todo/views.py
@@ -1,9 +1,9 @@
 from sqlite3 import IntegrityError
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.db import IntegrityError #preguntar porque
-from django.contrib.auth import login, logout
+from django.contrib.auth import login, logout, authenticate
 # Create your views here.
 
 
@@ -38,3 +38,13 @@ def logoutuser(request):
         logout(request)
         return redirect('home')
     
+def loginuser(request):
+    if request.method == 'GET':
+         return render(request, 'todo/loginuser.html',{'form':AuthenticationForm})
+    else:
+        user=authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'todo/loginuser.html',{'form':AuthenticationForm,'error':'Username and password incorrect'})
+        else:
+            login(request, user)
+            return redirect('currenttodos')
