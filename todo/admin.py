bd9288e72d4899b0a66574f00e4864277b910499
Fernan043 <feartag2710@gmail.com>
2025-03-27 22:52:43 -0600

    Todo Model completo y funcional mas primera relacion de tablas

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Todo
 # Register your models here.
+
+class TodoAdmin(admin.ModelAdmin):
+    readonly_fields= ('created',)
+
+
+#esto hace que cuando ingresamos a admin se vea el objeto añadido en este caso "Todo"
+admin.site.register(Todo,TodoAdmin)
--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# el import de User basicamente se utiliza como clave forenea para unir dos modelos
+# en SQL seria el igual de una clave foranea entre tablas
+#la variable user funciona de esa menera para realizar la conexion
+#cada usuario en django por defecto tienen un id la cual actua como una id de SQL
+#Investigar mas para el proyecto
+
+class Todo(models.Model):
+    title = models.CharField(max_length=100)
+    memo = models.TextField(blank=True)
+    created = models.DateTimeField(auto_now_add = True)
+    datecompleted = models.DateTimeField(null = True, blank=True)
+    important = models.BooleanField(default = False)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+
+def __str__(self):
+    return self.title
